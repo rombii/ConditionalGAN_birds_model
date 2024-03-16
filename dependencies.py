850ed6bc0c203fea3587ac502dc62e799b360fe8
ibmorr <jakubekk5@gmail.com>
2024-03-16 14:33:59 +0100

    Added checking if file exists in dependencies.py

--- dependencies.py
+++ dependencies.py
@@ -1,28 +1,26 @@
 from mega import Mega
 import zipfile
+import os
 
 mega_url = 'https://mega.nz/file/dEJVRYRT#zc_eWxp2GIKxMz8tHxsMcKLt5gnwHCh9o0_6UOXOTtk'
-
-
 zip_file_path = './dependencies.zip'
-
-
-extract_path = '.'
-
+extract_path = './'
 
 mega = Mega()
-
-
 m = mega.login()
 
+# Check if the zip file already exists
+if not os.path.exists(zip_file_path):
+    print("Downloading...")
+    try:
+        file = m.download_url(mega_url)
+    except PermissionError as e:
+        pass
 
-print("Downloading...")
-try:
-    file = m.download_url(mega_url)
-except PermissionError as e:
-    pass
-
-
-print("Extracting...")
-with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-    zip_ref.extractall(extract_path)
+# Check if the folder where you want to extract the zip file exists
+if not os.path.exists(os.path.join(extract_path, 'Data', 'CUB_200_2011_reformat', 'images')):
+    print("Extracting...")
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_path)
+else:
+    print("Folder already exists. Skipping extraction.")
